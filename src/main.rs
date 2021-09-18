876a788e325f63b8ce7d54e4205812251bf24202
Sam Yaple <sam+github@yaple.net>
2021-09-18 14:17:50 -0400

    filepath_handling

--- src/main.rs
+++ src/main.rs
@@ -15,22 +15,23 @@ fn help() {
 fn main() -> Result<(), pest::error::Error<Rule>> {
     let raw_args: Vec<String> = std::env::args().collect();
     let args = &raw_args.join(" ");
-    println!("{:?}", raw_args);
-    println!("{}", args);
 
     let pairs = OptParser::parse(Rule::options, args)?;
-    //println!("{:?}", pairs);
     for pair in pairs {
         match pair.as_rule() {
-            Rule::prog  => println!("Program: {}", pair.into_inner().as_str()),
             Rule::debug => println!("Debug is set"),
-            Rule::file  => {
-                let path = pair.into_inner().as_str();
-                println!("File path: {}", path)
-            },
             Rule::help  => help(),
+            Rule::prog  => {
+                let path = std::path::Path::new(pair.into_inner().as_str());
+                println!("Program: {}", path.display());
+            }
+            Rule::file  => {
+                let path = std::path::Path::new(pair.into_inner().as_str());
+                println!("File: {}", path.display());
+            },
             Rule::kvlist => {
                 for rule in pair.into_inner() {
+                    println!("Rule: {:?}", rule);
                     println!("kvpair found: {}", rule.as_str());
                 }
             },
