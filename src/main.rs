969597806a17cd7479cff89eef0928adc1949621
Sam Yaple <sam+github@yaple.net>
2021-09-17 21:56:39 -0400

    Playing with errors

--- src/main.rs
+++ src/main.rs
@@ -8,23 +8,26 @@ use pest::Parser;
 #[grammar = "opts.pest"]
 struct OptParser;
 
-fn main() {
+fn help() {
+    println!("Showing Help");
+}
+
+fn main() -> Result<(), pest::error::Error<Rule>> {
     let raw_args: Vec<String> = std::env::args().collect();
     // Drop the 0th element which is the program name in common situations
     let args = &raw_args[1..raw_args.len()].join(" ");
 
-    let pairs = OptParser::parse(Rule::options, args)
-        .unwrap_or_else(|e| panic!("{}", e));
-
+    let pairs = OptParser::parse(Rule::options, args)?;
     for pair in pairs {
         match pair.as_rule() {
             Rule::debug => println!("Debug is set"),
-            Rule::help  => println!("Showing Help"),
             Rule::file  => {
                 let path = pair.into_inner().as_str();
                 println!("File path: {:?}", path)
             },
-            _ => unreachable!()
+            Rule::help  => help(),
+            _ => help(),
         };
     }
+    Ok(())
 }
